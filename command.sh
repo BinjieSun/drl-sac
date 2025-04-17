ca50dcbcf0e3372305de39e553ef75d42d011233
SizheWei <swei@gatech.edu>
2025-04-17 17:41:44 -0400

    Delete unnecessary files.

--- command.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES=4 python train_1024_3e-4_s=1.py
-
-CUDA_VISIBLE_DEVICES=5 python train_1024_3e-4_s=42.py
-
-CUDA_VISIBLE_DEVICES=6 python train_4096_3e-4_s=1.py
-
-CUDA_VISIBLE_DEVICES=7 python train_4096_3e-4_s=42.py
